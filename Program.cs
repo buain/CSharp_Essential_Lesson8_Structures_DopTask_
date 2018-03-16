b3b30179bb4b0549d5749339c3e00c2fdc4b52ba
Vladimir <33662107+buain@users.noreply.github.com>
2018-03-16 23:26:14 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserBirthDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now; // Сегодняшняя дата
+            Console.WriteLine("Введите месяц своего рождения"); // Ввод пользователем месяц рождения
+            int y = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Введите день своего рождения"); // Ввод пользователем день рождения
+            int z = Int32.Parse(Console.ReadLine());
+            DateTime UserBirthDay = new DateTime(2018, y, z);  //Дата рождения пользователя
+            TimeSpan left = today - UserBirthDay;     // Дни до дня рождения
+            if (UserBirthDay < today)
+            {
+                Console.WriteLine("До дня рождения осталось " + (365 - left.Days) + " дней");
+            }
+            else 
+            Console.WriteLine("До дня рождения осталось "+ left.Days +" дней");
+            Console.ReadKey();
+        }
+    }
+}
